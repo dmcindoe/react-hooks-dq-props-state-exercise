b5988afaf9977785a6058843d87d6b5411a6c39e
dmcindoe <84344530+dmcindoe@users.noreply.github.com>
2021-07-06 09:52:22 -0400

    Update MasterHog.js

--- src/components/MasterHog.js
+++ src/components/MasterHog.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import Master from "../assets/master-hog.png";
 import BabyHog from "./BabyHog";
-// import offspring from "../data.js"
+
+import offspring from "../data.js"
 
 function MasterHog() {
   const [eyeColor, setEyeColor] = useState("blue");
@@ -40,12 +41,17 @@ function MasterHog() {
         <img id="master-blaster" src={Master} alt="" />
       </div>
       <ul className="hoglist">
-        <BabyHog />
-        <BabyHog />
-        <BabyHog />
+     
+          {offspring.map(hog => 
+            <BabyHog 
+            eyeColor = {eyeColor}
+            name = {hog.name}
+            hobby = {hog.hobby}
+            
+            />
+          )}
+        
       </ul>
     </div>
   );
 }
-
-export default MasterHog;
